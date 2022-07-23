afc21abf4da4df2ef234fd62ff13bcf268b35151
Floor Eijkelboom <36603702+FloorEijkelboom@users.noreply.github.com>
2022-07-23 14:20:30 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,8 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'nbsphinx'
+	'nbsphinx',
+	'sphinx.ext.mathjax',
 ]
 
 intersphinx_mapping = {
