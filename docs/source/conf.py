891a6daea68eddd3f1170a1cb1d849807d165323
Floor Eijkelboom <eijkelboomfloor@gmail.com>
2022-07-28 10:21:26 +0200

    fix typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # Custom css file added in _static
-html_css_files = ['_static']
+html_css_files = ['style.css']
 
 # -- Options for HTML output
 
